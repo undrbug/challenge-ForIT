22940ec323ef9258e3a329d0fab26be84c1cf495
hGauna <undrbug@gmail.com>
2025-05-13 21:37:28 -0300

    implementacion link al home del navbar

--- back/front/src/components/nav/NavBar.jsx
+++ back/front/src/components/nav/NavBar.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom'
 import './navStyle.css'
+
 
 const NavBar = () => {
   return (
     <div className="navbar">
         <div className="navbar__links">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
+            <Link to="/">Home</Link>
+            <Link to="#about">About</Link>
+            <Link to="#services">Services</Link>
             <a href="#contact">Contact</a>
         </div>     
     </div>
